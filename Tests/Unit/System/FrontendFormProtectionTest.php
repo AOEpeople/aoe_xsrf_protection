b9ea0c506a50f241d8f5b679df49b99440a91bb9
benedikt.ringlein <benedikt.ringlein@aoe.com>
2016-04-22 10:53:11 +0200

    [Add][WE-5973] unit tests for token validation
    https://jira.congstar.net/browse/WE-6002

--- Tests/Unit/System/FrontendFormProtectionTest.php
+++ Tests/Unit/System/FrontendFormProtectionTest.php
@@ -9,57 +9,108 @@
 
 class FrontendFormProtectionTest extends PHPUnit_Framework_TestCase
 {
+	const FORMNAME = "FORMNAME";
+	const FORMNAME2 = "YOURFORM";
 	/**
 	 * @var \Aoe\XsrfProtection\System\Typo3\FrontendFormProtection
 	 */
 	protected $frontendFormProtection;
 
 	/**
-	 * @var \TYPO3\CMS\Core\Authentication\AbstractUserAuthentication
+	 * @var
 	 */
 	protected $fe_user;
 
 	public function setUp()
 	{
-		$this->fe_user = $this->getMock('\TYPO3\CMS\Core\Authentication\AbstractUserAuthentication');
+		$GLOBALS['LANG'] = $this->getMock('\\TYPO3\\CMS\\Lang\\LanguageService');
+		$this->fe_user = $this->getMock('\\TYPO3\\CMS\Core\\Authentication\\AbstractUserAuthentication');
 		$this->frontendFormProtection = new \Aoe\XsrfProtection\System\Typo3\FrontendFormProtection($this->fe_user);
 	}
 
 	/**
-	 * Tests, if the same token is retrieved on subsequent calls
+	 * Tests, if the same token is retrieved on subsequent calls and the token
+	 * is stored in the form protection (-> getSessionData only called once).
 	 *
 	 * @test
 	 */
 	public function testSameSessionSameToken()
 	{
-		$firstToken = $this->frontendFormProtection->generateToken("FORMNAME");
-		$secondToken = $this->frontendFormProtection->generateToken("FORMNAME");
+		$this->fe_user->expects($this->once())->method('getSessionData');
+		$firstToken = $this->frontendFormProtection->generateToken(self::FORMNAME);
+		$secondToken = $this->frontendFormProtection->generateToken(self::FORMNAME);
 		$this->assertEquals($firstToken, $secondToken);
 	}
 
 	/**
-	 * Tests, if a different token is retrieved with different sessions
+	 * Tests, if a different token is retrieved with different sessions.
+	 * Since the form protection is created new, the session data has to be
+	 * retrieved twice from the user object.
 	 *
 	 * @test
 	 */
 	public function testDifferentSessionDifferentToken()
 	{
-		$firstToken = $this->frontendFormProtection->generateToken("FORMNAME");
+		$this->fe_user->expects($this->once())->method('getSessionData');
+		$firstToken = $this->frontendFormProtection->generateToken(self::FORMNAME);
 		$this->fe_user = $this->getMock('\TYPO3\CMS\Core\Authentication\AbstractUserAuthentication');
+		$this->fe_user->expects($this->once())->method('getSessionData');
 		$this->frontendFormProtection = new \Aoe\XsrfProtection\System\Typo3\FrontendFormProtection($this->fe_user);
-		$secondToken = $this->frontendFormProtection->generateToken("FORMNAME");
+		$secondToken = $this->frontendFormProtection->generateToken(self::FORMNAME);
 		$this->assertNotEquals($firstToken, $secondToken);
 	}
 
 	/**
-	 * Tests, if different tokens are retrieved on subsequent calls with different form names
+	 * Tests, if different tokens are retrieved on subsequent calls with different form names but the same
+	 * session token (-> getSessionData only called once).
 	 *
 	 * @test
 	 */
-	public function testDifferentFormnaemeDifferentToken()
+	public function testDifferentFormnameDifferentToken()
 	{
-		$firstToken = $this->frontendFormProtection->generateToken("MYFORM");
-		$secondToken = $this->frontendFormProtection->generateToken("YOURFORM");
+		$this->fe_user->expects($this->once())->method('getSessionData');
+		$firstToken = $this->frontendFormProtection->generateToken(self::FORMNAME);
+		$secondToken = $this->frontendFormProtection->generateToken(self::FORMNAME2);
 		$this->assertNotEquals($firstToken, $secondToken);
 	}
+
+	/**
+	 * Tests, if a correct token can be validated.
+	 *
+	 * @test
+	 */
+	public function testValidateCorrectToken()
+	{
+		$this->fe_user->expects($this->once())->method('getSessionData');
+		$token = $this->frontendFormProtection->generateToken(self::FORMNAME);
+		$valid = $this->frontendFormProtection->validateToken($token, self::FORMNAME);
+		$this->assertTrue($valid);
+	}
+
+	/**
+	 * Tests, if an incorrect token can not be validated.
+	 *
+	 * @test
+	 */
+	public function testDontValidateIncorrectToken()
+	{
+		$this->fe_user->expects($this->once())->method('getSessionData');
+		$token = $this->frontendFormProtection->generateToken(self::FORMNAME);
+		$valid = $this->frontendFormProtection->validateToken("00000000000000000000000000000000", self::FORMNAME);
+		$this->assertFalse($valid);
+	}
+
+	/**
+	 * Tests, if a correct token cant be validated with a wrong form name.
+	 *
+	 * @test
+	 */
+	public function testDontValidateCorrectTokenIncorrectFormname()
+	{
+		$this->fe_user->expects($this->once())->method('getSessionData');
+		$token = $this->frontendFormProtection->generateToken(self::FORMNAME);
+		$valid = $this->frontendFormProtection->validateToken($token, self::FORMNAME2);
+		$this->assertFalse($valid);
+	}
+
 }
